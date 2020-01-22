523577d72c8f9f54c3d5315a0f4833302f1c4ad1
k3rn3lhack3r <k3rn3lhack3r@gmail.com>
2020-01-22 16:28:59 +0000

    Added hello.c source file

---
+++ hello.c
@@ -0,0 +1,10 @@
+/*
+* Hello World example program for a Twitter mate.
+* Compile with: gcc hello.c -o hello
+* Run with: ./hello
+*/
+#include <stdio.h>
+int main(void) {
+	printf("Hello, world!\n");
+	return 0;
+}
